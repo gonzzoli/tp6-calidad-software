d7e820c0feed1612dc088c8022f294ddec988e79
gonzalo <gonzalo@localhost.localdomain>
2024-09-05 18:49:52 -0300

    primer commit, los mejores testers

---
+++ cypress/e2e/prueba-registro.cy.js
@@ -0,0 +1,21 @@
+describe("registro de usuario", () => {
+  beforeEach(() => {
+    cy.visit("https://thinking-tester-contact-list.herokuapp.com");
+  });
+
+  it("clickear boton Sign Up redirige a pantalla de registro", () => {
+    cy.get("#signup").click();
+    cy.url().should("include", "/addUser");
+  });
+
+  it("usuario completa registro correctamente", () => {
+    cy.visit("https://thinking-tester-contact-list.herokuapp.com/addUser");
+    cy.get("#firstName").type("juan");
+    cy.get("#lastName").type("perez");
+    const numAleatorio = Math.round(Math.random()*100);
+    cy.get("#email").type(`juanperez${numAleatorio}@gmail.com`);
+    cy.get("#password").type("contrasenia123");
+    cy.get("#submit").click();
+    cy.url().should('include', "/contactList")
+  });
+});
