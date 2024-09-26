e69151ae3c43cf9a4645b240ae9c6ab724c4e7e0
Gonzalo Pozzoli <gonzalopozzoli2@gmail.com>
2024-09-26 12:17:54 -0300

    prueba pull-req para workflow

--- src/componentes/ModalLogin.tsx
+++ src/componentes/ModalLogin.tsx
@@ -4,7 +4,15 @@ export default function ModalLogin({ login }: { login: (usuario: string) => void
   const [usuario, setUsuario] = useState("");
 
   return (
-    <div style={{background: "white"}}>
+    <div
+      style={{
+        background: "white",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        padding: "3rem",
+      }}>
       <label htmlFor="titulo">Usuario</label>
       <input type="text" id="titulo" onChange={(e) => setUsuario(e.target.value)} />
       <button onClick={() => login(usuario)}>Iniciar sesion</button>
